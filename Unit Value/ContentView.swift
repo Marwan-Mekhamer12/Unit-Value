dddd838016a8f68565c31fa2b16945611035eb15
Marwan Mekhamer <Marwan-Mekhamer12@gmail.com>
2026-04-09 12:25:27 -0700

    Initial Commit

---
+++ Unit Value/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Unit Value
+//
+//  Created by Marwan Mekhamer on 09/04/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Unit Value/Unit_ValueApp.swift
@@ -0,0 +1,17 @@
+//
+//  Unit_ValueApp.swift
+//  Unit Value
+//
+//  Created by Marwan Mekhamer on 09/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct Unit_ValueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
